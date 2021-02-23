4349ea533d5faee5ca89608825a624510c19ae52
gabbelabbe <gabbe.stark@gmail.com>
2021-02-23 16:50:22 +0100

    Mades some stuff lmao

--- server/index.js
+++ server/index.js
@@ -1,10 +1,35 @@
-const express = require('express')
+import express from 'express'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import mongoose from 'mongoose'
 
 const app = express()
+app.use(helmet())
+app.use(morgan('common'))
 app.use(express.json())
 
-app.post('/', (req, res) => {
-  res.send('Hello ' + req.body.name)
+const isAdmin = (req, res, next) => {
+  console.log('is admin')
+  next()
+}
+
+app.get('/', (req, res) => {
+  res.send('yeetus')
 })
 
-app.listen(3001)
+app.get('/dice', isAdmin, (req, res) => {
+  res.send(Math.round(Math.random() * 6).toString())
+})
+
+mongoose.connect('mongodb://localhost/namndb', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('connected to db'))
+  .catch((err) => {
+    console.error(err)
+    process.exit()
+  })
+
+app.listen(3001, () => {
+  console.log('listening on port ' + 3001)
+})
+
+export default app
