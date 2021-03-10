3c8d45903cf6bc1e0ac10785afe5e4ac1bc5dcbe
gabbelabbe <gabbe.stark@gmail.com>
2021-03-10 14:18:58 +0100

    with credentials

--- client/src/shared/api/apiHandler.ts
+++ client/src/shared/api/apiHandler.ts
@@ -1,74 +1,72 @@
 import Axios from 'axios'
 import { iloginCredentials, iProduct, iStarWarsCharacters } from '../interface/states'
 
-const axiosInstance = Axios.create({
-  withCredentials: true
-})
+Axios.defaults.withCredentials = true
 const baseStarWarsUrl = 'https://swapi.dev/api/'
 const baseUrl = process.env.REACT_APP_SERVER_BASE_URL
 
 export const getPerson = (id: number | string): Promise<iStarWarsCharacters> => {
-  return axiosInstance.get(baseStarWarsUrl + 'people/' + id + '/')
+  return Axios.get(baseStarWarsUrl + 'people/' + id + '/')
     .then(res => res.data)
     .catch(err => console.error(err))
 }
 
 export const getMoreInfo = (url: string) => {
-  return axiosInstance.get(url)
+  return Axios.get(url)
     .then(res => res.data)
     .catch(err => console.error(err))
 }
 
 export const signUp = (email: string, username: string, password: string) => {
-  return axiosInstance.post(baseUrl + 'user/create', {email, username, password})
+  return Axios.post(baseUrl + 'user/create', {email, username, password})
     .then(res => res)
     .catch(err => console.error(err))
 }
 
 export const signIn = (username: string, password: string) => {
-  return axiosInstance.post(baseUrl + 'user/login', {username, password})
+  return Axios.post(baseUrl + 'user/login', {username, password})
     .then(res => res)
     .catch(err => console.error(err))
 }
 
 export const signOut = () => {
-  return axiosInstance.post(baseUrl + 'user/logout')
+  return Axios.post(baseUrl + 'user/logout')
     .then(res => res)
     .catch(err => console.error(err))
 }
 
 export const getAllUsers = () => {
-  return axiosInstance.get(baseUrl + 'users')
+  return Axios.get(baseUrl + 'users')
     .then(res => res)
     .catch(err => console.error(err))
 }
 
 export const getAllProducts = () => {
-  return axiosInstance.get(baseUrl + 'products')
+  return Axios.get(baseUrl + 'products')
     .then(res => res)
     .catch(err => console.error(err))
 }
 
 export const createProduct = (productInfo: iProduct) => {
-  return axiosInstance.post(baseUrl + 'product', {...productInfo})
+  return Axios.post(baseUrl + 'product', {...productInfo})
     .then(res => res)
     .catch(err => console.error(err))
 }
 
 export const updateProduct = (productInfo: iProduct) => {
-  return axiosInstance.put(baseUrl + 'product', {...productInfo})
+  return Axios.put(baseUrl + 'product', {...productInfo})
     .then(res => res)
     .catch(err => console.error(err))
 }
 
 export const deleteProduct = (_id: string) => {
-  return axiosInstance.delete(baseUrl + 'product', { data: {_id: _id} })
+  return Axios.delete(baseUrl + 'product', { data: {_id: _id} })
     .then(res => res)
     .catch(err => console.error(err))
 }
 
 export const updateUser = (userInfo: iloginCredentials) => {
-  return axiosInstance.put(baseUrl + 'user/change', {...userInfo})
+  return Axios.put(baseUrl + 'user/change', {...userInfo})
     .then(res => res)
     .catch(err => console.error(err))
 }
