65bf4c7ddb2d8cc0db698025a68265a0c2f776fd
gabbelabbe <gabbe.stark@gmail.com>
2021-03-10 16:58:09 +0100

    maybe session saves now?

--- server/src/controllers/User.controller.js
+++ server/src/controllers/User.controller.js
@@ -16,6 +16,10 @@ const createUser = async (req, res) => {
     const dbRes = await user.save()
     const serverUser = await UserModel.findById(dbRes._id, 'username email address userType')
     req.session.user = serverUser
+    req.session.save((err) => {
+      if (err)
+        console.error(err)
+    })
     res.status(201).send(serverUser)
   } catch (err) {
     res.status(500).send({
@@ -72,6 +76,10 @@ const loginUser = async (req, res) => {
     if (validPassword) {
       const signedInUser = await UserModel.findById(user._id, 'username email address userType')
       req.session.user = signedInUser
+      req.session.save((err) => {
+        if (err)
+          console.error(err)
+      })
       return res.status(200).send(signedInUser)
     } else {
       return res.status(400).send({msg: 'Incorrect password or username'})
@@ -99,6 +107,10 @@ const updateUser = async (req, res) => {
 const signOut = async (req, res) => {
   try {
     req.session.user = null
+    req.session.save((err) => {
+      if (err)
+        console.error(err)
+    })
     res.status(200).send('Logged Out')
   } catch (err) {
     res.status(500).send({
