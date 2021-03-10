7a8864d72b3c475ed338908157ceea02c6f23dd2
gabbelabbe <gabbe.stark@gmail.com>
2021-03-10 17:10:06 +0100

    :)))))

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   store: Configurations.store,
-  cookie: { maxAge: 3600000, httpOnly: true, secure: process.env.ENVIROMENT !== 'dev', sameSite: false },
+  cookie: { secure: process.env.ENVIROMENT !== 'dev' },
 }))
 app.use(helmet())
 app.use(morgan('common'))
